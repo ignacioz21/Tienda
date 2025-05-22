d07809497c0f8cd65618579fe398d4ba7f49ff38
ignacioz21 <168688549+ignacioz21@users.noreply.github.com>
2025-05-21 19:16:02 -0600

    Build Tienda v1.0

---
+++ Tienda_AndreMartinez/src/main/java/Tienda/Web/TiendaAndresMartinezApplication.java
@@ -0,0 +1,13 @@
+package Tienda.Web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaAndresMartinezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaAndresMartinezApplication.class, args);
+	}
+
+}
